06dc17f24369eff7ed784be53da6c2fc49d4926f
montasir12345 <155537115+montasir12345@users.noreply.github.com>
2024-02-05 14:03:57 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,23 @@
+      // object //
+ //let user = {
+ //   name : 'Masum Billah',
+ //   email : 'masum@gmail.com'
+// }
+  //  document.write(user.email);  
+
+
+  // document.getElementById('h1').innerText = 'hello bangladash'
+
+ // document.getElementById('h1').style.color = 'red'
+// document.querySelector('h1').style.color = 'red'
+
+
+//document.querySelector('button').addEventListener('click' , function(){
+  //  document.querySelector('h1').style.color = 'red';
+  //  document.querySelector('h1').innerText = 'hello bangladash';
+    
+//})
+document.querySelector('button').addEventListener('click' , function(){
+   document.querySelector('h1').classList.add('hello');
+   
+})
